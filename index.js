3677b019a2d8349ace5ba9e233ce6615fdc13be9
farshad-vgr <farshadvaghari@gmail.com>
2023-02-10 19:53:34 +0330

    Comments added

--- index.js
+++ index.js
@@ -1,121 +1,160 @@
 const tutorialSection = document.getElementById("tutorial-section");
 const tutorialBtn = document.getElementById("tutorial-btn");
+const levelSection = document.getElementById("level-section");
 const easyBtn = document.getElementById("easy-btn");
 const normalBtn = document.getElementById("normal-btn");
 const hardBtn = document.getElementById("hard-btn");
+const grid = document.getElementById("grid");
+const resultsDisplay = document.getElementById("results");
 
-const grid = document.querySelector(".grid");
-const resultsDisplay = document.querySelector(".results");
+// The level buttons are disabled at first
+for (const button of levelSection.querySelectorAll("button")) {
+  button.disabled = true;
+}
 
-const backgroundMusic = new sound("assets/sounds/bgMusic.wav");
-const startMusic = new sound("assets/sounds/start.mp3");
-const invadersMoveMusic = new sound("assets/sounds/invadersMove.mp3");
-const shootMusic = new sound("assets/sounds/shoot.wav");
-const explodeMusic = new sound("assets/sounds/explode.wav");
-const winMusic = new sound("assets/sounds/win.mp3");
-const gameOverMusic = new sound("assets/sounds/gameOver.mp3");
+// A class to build sound objects
+class Sound {
+  constructor(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+  }
+  playSound() {
+    this.sound.play();
+  }
+  stopSound() {
+    this.sound.pause();
+  }
+}
 
-let results = 0;
-const width = 15;
+// Building objects from the Sound class with a proper sound source for different parts of the game
+const backgroundSound = new Sound("assets/sounds/bgSound.wav");
+const startSound = new Sound("assets/sounds/start.mp3");
+const invadersMoveSound = new Sound("assets/sounds/invadersMove.mp3");
+const shootSound = new Sound("assets/sounds/shoot.wav");
+const explodeSound = new Sound("assets/sounds/explode.wav");
+const winSound = new Sound("assets/sounds/win.mp3");
+const gameOverSound = new Sound("assets/sounds/gameOver.mp3");
+
+let scores = 0; // A variable to hold the game score
+const width = 15; // Number of cells in each row and column
 let currentShooterIndex;
-let direction = 1;
-let isGoingRight = true;
-let aliensInterval;
+let direction = 1; // Invader's movement direction(1 for moving to the right and -1 for left)
+let isGoingRight = true; // Invader's movement direction
+let aliensInterval; // This variable is to clear the interval effect
+// An array of starting positions for aliens
 const fixedAlienPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
 let alienPositions = [...fixedAlienPositions];
-let alienRemovedPositions = [];
+let alienPositionsLength = alienPositions.length;
+let alienRemovedPositions = []; // Alien remove positions will push to this array
 
+// Fill the gameboard with divs and the total number of divs is 15*15
 for (let i = 0; i < width ** 2; i++) {
   grid.appendChild(document.createElement("div"));
 }
 
+// Each div in the gameboard is a square, using a spread operator to put these divs in an array
 const squares = [...document.querySelectorAll(".grid div")];
 
-for (let i = 0; i < alienPositions.length; i++) {
+// Aliens starting positions will be pushed to selected square(selected index)
+for (let i = 0; i < alienPositionsLength; i++) {
   squares[alienPositions[i]].classList.add("invader");
 }
 
 tutorialBtn.addEventListener("click", () => {
   tutorialSection.style.display = "none";
-  backgroundMusic.play();
+
+  // The level buttons will be enabled
+  for (const button of levelSection.querySelectorAll("button")) {
+    button.disabled = false;
+  }
+
+  // This function adds a listener to each game-level button
+  function eventListenerAdder(btn, time) {
+    btn.addEventListener("click", () => {
+      // The app listens to user actions when a key is pressed and then calls the proper function
+      document.addEventListener("keydown", moveShooter);
+      document.addEventListener("keydown", shoot);
+      startGame(time); // Each game runs with a time parameter to determine the alien's movement speed
+      startSound.playSound();
+    });
+  }
+  eventListenerAdder(easyBtn, 1000); // Set a listener for the easy level button
+  eventListenerAdder(normalBtn, 600); // Set a listener for the normal level button
+  eventListenerAdder(hardBtn, 300); // Set a listener for the hard level button
+
+  backgroundSound.playSound(); // Playing sound in the background needs the user interaction
 });
 
-easyBtn.addEventListener("click", () => {
-  document.addEventListener("keydown", moveShooter);
-  document.addEventListener("keydown", shoot);
-  startGame(1000);
-  startMusic.play();
-});
-normalBtn.addEventListener("click", () => {
-  document.addEventListener("keydown", moveShooter);
-  document.addEventListener("keydown", shoot);
-  startGame(600);
-  startMusic.play();
-});
-hardBtn.addEventListener("click", () => {
-  document.addEventListener("keydown", moveShooter);
-  document.addEventListener("keydown", shoot);
-  startGame(300);
-  startMusic.play();
-});
-
+// The app listens to user actions when a key is pressed and then calls the proper function
 document.addEventListener("keydown", moveShooter);
 document.addEventListener("keydown", shoot);
 
+// This function makes the shooter move left or right with arrows on the keyboard
 function moveShooter(e) {
   switch (e.key) {
     case "ArrowLeft":
+      // First, check if the shooter has an empty cell on the left side then it can move
       if (currentShooterIndex % width !== 0) {
-        squares[currentShooterIndex].classList.remove("shooter");
-        currentShooterIndex--;
-        squares[currentShooterIndex].classList.add("shooter");
+        squares[currentShooterIndex].classList.remove("shooter"); // Remove shooter from current cell
+        squares[--currentShooterIndex].classList.add("shooter"); // Add shooter to this cell
       }
       break;
     case "ArrowRight":
+      // First, check if the shooter has an empty cell on the right side then it can move
       if (currentShooterIndex % width < width - 1) {
-        squares[currentShooterIndex].classList.remove("shooter");
-        currentShooterIndex++;
-        squares[currentShooterIndex].classList.add("shooter");
+        squares[currentShooterIndex].classList.remove("shooter"); // Remove shooter from current cell
+        squares[++currentShooterIndex].classList.add("shooter"); // Add shooter to this cell
       }
       break;
   }
 }
 
+// This function handle shoots
 function shoot(e) {
+  // A shoot will be executed when the up arrow of the keyboard press
   if (e.key === "ArrowUp") {
-    shootMusic.play();
     let currentLaserIndex = currentShooterIndex;
-    const laserInterval = setInterval(moveLaser, 100);
+    const laserInterval = setInterval(moveLaser, 100); // The laser position will be updated each 100 ms
+    shootSound.playSound();
 
+    // This function updates the shooter's laser index from bottom of the gameboard to top
     function moveLaser() {
       squares[currentLaserIndex].classList.remove("laser");
-      currentLaserIndex -= width;
+      currentLaserIndex -= width; // The laser's position moves up one row
 
       if (currentLaserIndex < 0) {
-        clearInterval(laserInterval);
+        clearInterval(laserInterval); // If the laser reached the top of the gameboard it will be stopped
       } else {
         squares[currentLaserIndex].classList.add("laser");
 
+        // If the laser hits an alien this block will be executed
         if (squares[currentLaserIndex].classList.contains("invader")) {
-          squares[currentLaserIndex].classList.remove("laser");
-          squares[currentLaserIndex].classList.remove("invader");
-          squares[currentLaserIndex].classList.add("boom");
-          explodeMusic.play();
+          squares[currentLaserIndex].classList.remove("laser"); // Remove laser shape
+          squares[currentLaserIndex].classList.remove("invader"); // Remove alien shape
+          squares[currentLaserIndex].classList.add("boom"); // Add an explode shape
+          explodeSound.playSound();
 
+          // Remove explode shape after 200 ms to clean the cell
           setTimeout(() => squares[currentLaserIndex].classList.remove("boom"), 200);
 
-          clearInterval(laserInterval);
+          clearInterval(laserInterval); // Clear the interval after explosion
 
-          alienRemovedPositions.push(alienPositions.indexOf(currentLaserIndex));
+          alienRemovedPositions.push(alienPositions.indexOf(currentLaserIndex)); // Remove alien
 
-          results++;
+          scores++;
 
-          resultsDisplay.innerHTML = "Score: " + results;
+          resultsDisplay.innerHTML = "Score: " + scores + "/30";
 
-          if (results === 30) {
+          // End of the game when all 30 aliens were removed
+          if (scores === 30) {
             resultsDisplay.innerHTML = `<span style="color: lightgreen">"YOU WON!"</span>`;
-            winMusic.play();
-            clearInterval(aliensInterval);
+            winSound.playSound();
+            clearInterval(aliensInterval); // Stop the movement of aliens
+
+            // The app does not listen to user actions anymore
             document.removeEventListener("keydown", moveShooter);
             document.removeEventListener("keydown", shoot);
           }
@@ -125,66 +164,84 @@ function shoot(e) {
   }
 }
 
+// Start a new game with default values(restart function)
 function startGame(level) {
-  results = 0;
-  resultsDisplay.innerHTML = "Score: " + results;
+  scores = 0;
+  resultsDisplay.innerHTML = "Score: " + scores + "/30";
   currentShooterIndex = width ** 2 - Math.ceil(width / 2);
   direction = 1;
   isGoingRight = true;
   clearInterval(aliensInterval);
   alienPositions = [...fixedAlienPositions];
+  alienPositionsLength = alienPositions.length;
   alienRemovedPositions.splice(0);
 
+  // Remove all shapes from the gameboard
   for (let i = 0; i < squares.length; i++) {
     squares[i].classList.remove("invader", "shooter", "laser", "boom");
   }
 
-  for (let i = 0; i < alienPositions.length; i++) {
+  // Aliens starting positions will be pushed to selected square(selected index)
+  for (let i = 0; i < alienPositionsLength; i++) {
     squares[alienPositions[i]].classList.add("invader");
   }
 
+  // Add shooter to the bottom of the gameboard
   squares[currentShooterIndex].classList.add("shooter");
 
+  // This interval updates the position of aliens depending on the game level
   aliensInterval = setInterval(moveAliens, level);
 
+  // This function handles alien's movements
   function moveAliens() {
-    const leftEdge = alienPositions[0] % width === 0;
-    const rightEdge = alienPositions[alienPositions.length - 1] % width === width - 1;
+    const isOnLeftEdge = alienPositions[0] % width === 0;
+    const isOnRightEdge = alienPositions[alienPositionsLength - 1] % width === width - 1;
 
-    for (let i = 0; i < alienPositions.length; i++) {
+    // This loop hides removed aliens
+    for (let i = 0; i < alienPositionsLength; i++) {
       if (!alienRemovedPositions.includes(i)) {
         squares[alienPositions[i]].classList.remove("invader");
       }
     }
 
-    if (rightEdge && isGoingRight) {
-      invadersMoveMusic.play();
-      for (let i = 0; i < alienPositions.length; i++) {
+    // Change in movement direction(from right to left) depends on the alien's position
+    if (isOnRightEdge && isGoingRight) {
+      invadersMoveSound.playSound();
+
+      // All the alien's position moves down one row
+      for (let i = 0; i < alienPositionsLength; i++) {
         alienPositions[i] += width + 1;
         direction = -1;
         isGoingRight = false;
       }
     }
 
-    if (leftEdge && !isGoingRight) {
-      invadersMoveMusic.play();
-      for (let i = 0; i < alienPositions.length; i++) {
+    // Change in movement direction(from left to right) depends on the alien's position
+    if (isOnLeftEdge && !isGoingRight) {
+      invadersMoveSound.playSound();
+
+      // All the alien's position moves down one row
+      for (let i = 0; i < alienPositionsLength; i++) {
         alienPositions[i] += width - 1;
         direction = 1;
         isGoingRight = true;
       }
     }
 
-    for (let i = 0; i < alienPositions.length; i++) {
+    // Changing alien's current cell index one by one(to right or left depends on direction value)
+    for (let i = 0; i < alienPositionsLength; i++) {
       alienPositions[i] += direction;
     }
 
-    for (let i = 0; i < alienPositions.length; i++) {
+    // If the aliens reached the down of the gameboard the game will be over
+    for (let i = 0; i < alienPositionsLength; i++) {
       if (!alienRemovedPositions.includes(i)) {
         if (alienPositions[i] > 209) {
           resultsDisplay.innerHTML = `<span style="color: red">"GAME OVER!"</span>`;
-          gameOverMusic.play();
-          clearInterval(aliensInterval);
+          gameOverSound.playSound();
+          clearInterval(aliensInterval); // Stop the movement of aliens
+
+          // The app does not listen to user actions anymore
           document.removeEventListener("keydown", moveShooter);
           document.removeEventListener("keydown", shoot);
         }
@@ -192,26 +249,17 @@ function startGame(level) {
       }
     }
 
+    // If the aliens hit the shooter the game will be over
     if (squares[currentShooterIndex].classList.contains("invader", "shooter")) {
       resultsDisplay.innerHTML = `<span style="color: red">"GAME OVER!"</span>`;
-      gameOverMusic.play();
-      clearInterval(aliensInterval);
+      gameOverSound.playSound();
+      clearInterval(aliensInterval); // Stop the movement of aliens
+
+      squares[currentShooterIndex].classList.add("boom"); // Shooter exploded shape
+
+      // The app does not listen to user actions anymore
       document.removeEventListener("keydown", moveShooter);
       document.removeEventListener("keydown", shoot);
     }
   }
-}
-
-function sound(src) {
-  this.sound = document.createElement("audio");
-  this.sound.src = src;
-  this.sound.setAttribute("preload", "auto");
-  this.sound.style.display = "none";
-  document.body.appendChild(this.sound);
-  this.play = function () {
-    this.sound.play();
-  };
-  this.stop = function () {
-    this.sound.pause();
-  };
 }
